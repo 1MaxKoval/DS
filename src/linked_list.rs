e34c96e85ad6dcb286732f2c04bedba27fa02ffd
max_k <koval6813@gmail.com>
2023-06-29 13:04:16 +0200

    Isolated LinkedList

---
+++ src/linked_list.rs
@@ -0,0 +1,109 @@
+
+struct Node<T> {
+    value: T,
+    next_node: Option<Box<Node<T>>>
+}
+
+struct LinkedList<T> {
+    head_node: Option<Box<Node<T>>>,
+    length: usize
+}
+
+impl<T> LinkedList<T> {
+
+    pub fn new(data: T) -> Self {
+        let hn = Node {
+            value: data,
+            next_node: None
+        };
+        LinkedList {
+            head_node: Some(Box::new(hn)),
+            length: 1
+        }
+    }
+
+    pub fn push(&mut self, data: T) { // TODO: Combine push and add_node functions!
+        let n = Node {
+            value: data,
+            next_node: Option::None
+        };
+        match &mut self.head_node {
+            Some(node) => {
+                Self::add_node(node, n);
+            },
+            None => {
+                self.head_node = Some(Box::new(n));
+            }
+        }
+        self.length += 1;
+    }
+
+    pub fn get(&self, i: usize) -> &T { 
+        if i >= self.length {
+            panic!("LinkedList: index out of range!")
+        }
+        match &self.head_node {
+            Some(node) => { &Self::get_node(node, i).value }
+            None => { panic!("No elements present in the linked list!") }
+        }
+    }
+
+    pub fn pop() {} // Extend from delete
+
+    pub fn delete(&mut self, i: usize) {}
+
+    pub fn insert(&mut self, data: T, i: usize) {
+        if i >= self.length {
+            panic!("LinkedList: index out of range!")
+        }
+        if self.length == 0 {
+            panic!("Linkedlist: Cannot insert a value into an empty LinkedList!")
+        }
+
+        if i == 0 {
+            let n = self.head_node.take();
+            let new_node = Node { value: data, next_node: n };
+            self.head_node.replace(Box::new(new_node));
+        }
+        else {
+            let mut current = self.head_node.as_mut().unwrap(); // Panicked earlier anyway
+            for _ in 0..i {
+                current = current.next_node.as_mut().unwrap();
+            }
+            let n = current.next_node.take();
+            let new_node = Node { value: data, next_node: n };
+            current.next_node.replace(Box::new(new_node));
+        }
+
+        self.length += 1;
+    }
+
+    fn add_node(head_node: &mut Node<T>, new_node: Node<T>) {
+        // Assumes head_node exists
+        let mut prev = head_node;
+        loop {
+            let next_node = &mut prev.next_node;
+            if let Some(node) = next_node {
+                prev = node;
+            } else {
+                *next_node = Some(Box::new(new_node));
+                break;
+            }
+        }
+    }
+
+    fn get_node(head_node: &Node<T>, i: usize) -> &Node<T> {
+        // Assumes head_node exists
+        let mut prev = head_node;
+        for _ in 0..(i+1) {
+            match &prev.next_node {
+                Some(node) => prev = node,
+                None => return prev
+            }
+        }
+        prev
+    }
+
+
+}
+
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 mod master_slave;
+mod linked_list;
 use master_slave::run;
 
 struct Node {
@@ -6,33 +7,40 @@ struct Node {
     next_node: Option<Box<Node>>
 }
 
+struct LinkedList {}
 
-// fn add_node(head_node: &mut Node, new_node: Node) {
-//     let mut prev = head_node;
-//     loop {
-//         if let Some(node) = &mut prev.next_node {
-//             prev = node;
-//         } else {
-//             prev.next_node = Some(Box::new(new_node));
-//             break;
-//         }
-//     }
-// }
 
-// fn get_mut(head_node: &mut Node, i: usize) -> &mut Node {
-//     let mut prev = head_node;
-//     for p in 0..(i+1) {
-//         match &mut prev.next_node {
-//             Some(node) => prev = node,
-//             None => return prev
-//         }
-//     }
-//     prev
-// }
+fn add_node(head_node: &mut Node, new_node: Node) {
+    let mut prev = head_node;
+    loop {
+        let next_node = &mut prev.next_node;
+        if let Some(node) = next_node {
+            prev = node;
+        } else {
+            *next_node = Some(Box::new(new_node));
+            break;
+        }
+    }
+}
+
+fn get_mut(head_node: &mut Node, i: usize) -> &mut Node {
+    let mut current = Some(head_node);
+    for _ in 0..(i+1) {
+        if let Some(node) = current {
+            current = node.next_node.as_deref_mut();
+        } else { break; }
+    }
+
+    match current {
+        Some(node) => return node,
+        None => panic!("cannot happen")
+    }
+}
+
 
 fn get(head_node: &Node, i: usize) -> &Node {
     let mut prev = head_node;
-    for p in 0..(i+1) {
+    for _ in 0..(i+1) {
         match &prev.next_node {
             Some(node) => prev = node,
             None => return prev
@@ -44,7 +52,8 @@ fn get(head_node: &Node, i: usize) -> &Node {
 fn delete_node(head_node: Node, i: usize) {
 }
 
-fn insert_node(head_node: Node) {}
+fn insert_node(head_node: Node) {
+}
 
 fn main() {
     run();
